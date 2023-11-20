8f85f2d6c17af06f21dced19c8c2200071274d3c
Ford su <ford.s@trevi.cc>
2023-11-20 18:25:24 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module webcrawl
+
+go 1.21.4
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	// "net/http"
+	// "time"
+
+	"webcrawl/config"
+	// "./models"
+	// "./utils"
+)
+
+func main() {
+	// Load configuration from config.yaml file
+	config, err := config.LoadConfig()
+	// if err != nil {
+	// 	log.Fatal("Error loading config:", err)
+	// }
+
+	// // Create a new crawler instance
+	// crawler := utils.NewCrawler(config)
+
+	// // Start crawling the target URL
+	// err = crawler.Crawl(config.TargetURL)
+	// if err != nil {
+	// 	log.Fatal("Error crawling:", err)
+	// }
+
+	// // Print the crawled items
+	// for _, item := range crawler.Items {
+	// 	fmt.Printf("%s - %s\n", item.Title, item.URL)
+	// }
+}
