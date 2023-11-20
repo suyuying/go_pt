18e53aff368c736fedebd335f0e5c5dc3125cb40
Ford su <ford.s@trevi.cc>
2023-11-20 19:00:14 +0800

    init

--- main.go
+++ main.go
@@ -2,18 +2,40 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"github.com/gocolly/colly/v2"
+	"gopkg.in/yaml.v3"
+	"os"
 	// "net/http"
 	// "time"
-
-	"webcrawl/config"
+	// "webcrawl/config"
 	// "./models"
 	// "./utils"
 )
 
 func main() {
+	configData, err := os.ReadFile("config/config.yaml")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	type Config struct {
+		TargetURL string `yaml:"target_url"`
+		MaxDepth  int    `yaml:"max_depth"`
+	}
+	// 將 configData 解析為 YAML 格式的結構體
+	var config Config
+	err = yaml.Unmarshal(configData, &config)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// 使用 config 結構體
+	fmt.Println(config.TargetURL) // https://www.example.com
+	fmt.Println(config.MaxDepth)  // 2
+
 	// Load configuration from config.yaml file
-	config, err := config.LoadConfig()
+	// config, err := config.LoadConfig()
 	// if err != nil {
 	// 	log.Fatal("Error loading config:", err)
 	// }
